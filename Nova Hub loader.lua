f6f9ad21903581e675febc05d9c1fc7d8f692e8f
Encrypted <marwanlecourtabram91670@gmail.com>
2025-04-01 17:07:12 +0200

    Update Nova Hub loader.lua

--- Nova Hub loader.lua
+++ Nova Hub loader.lua
@@ -1,1 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/EncryptedV10/Nova-Hub/refs/heads/main/Gamelist.lua"))()
 
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
