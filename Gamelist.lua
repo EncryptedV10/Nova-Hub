45a1e06bf75bde3524a8444cdc4c9362b99698e3
Encrypted <marwanlecourtabram91670@gmail.com>
2025-04-01 17:01:05 +0200

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,1 +1,6 @@
+local Games = {
+  [3623096087] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/EncryptedV2/Free/refs/heads/main/Key%20System", true))()", -- Muscle Legends
+  [1119466531] = "https://pastebin.com/raw/pe5CGnu6", -- Legends of Speed
+}
 
+return Games
