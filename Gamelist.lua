858540f37f461167d3c1a926e483cc881d3a6ec2
Encrypted <marwanlecourtabram91670@gmail.com>
2025-04-22 13:16:32 +0200

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,6 +1,6 @@
 local Games = {
   [3623096087] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/Muscle%20Legends.lua", -- Muscle Legends
-  [1119466531] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/Legends%20Of%20Speed.lua", -- Legends of Speed
+  [3101667897] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/Legends%20Of%20Speed.lua", -- Legends of Speed
 }
 
 return Games
