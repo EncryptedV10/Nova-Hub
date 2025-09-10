d90012a269be06d96753d815136bb99b6aae1963
Encrypted <marwanlecourtabram91670@gmail.com>
2025-09-10 04:18:16 +0200

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -2,6 +2,7 @@ local Games = {
   [3623096087] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/Muscle%20Legends.lua", -- Muscle Legends
   [3101667897] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/Legends%20Of%20Speed.lua", -- Legends of Speed
   [126884695634066] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/Grow%20A%20Garden.lua", -- Grow A Garden
+  [10449761463] = "https://raw.githubusercontent.com/EncryptedV10/Roblox-Scripts/refs/heads/main/The%20Strongest%20Battleground.lua", -- The Strongest Battle Ground
   [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 2
   [7449423635] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 3
